41f86ef1129304eba471bfcabe5be47534967642
wisdigitex <sulaimonganiyy3500@gmail.com>
2026-05-23 15:00:51 -0600

    Fix admin pages and build scripts

---
+++ app/admin/projects/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AdminProjectsPage() {
+  return (
+    <main className="min-h-screen bg-[#050816] text-white p-10">
+      <h1 className="text-4xl font-black">
+        Admin Projects
+      </h1>
+    </main>
+  );
+}
---
+++ app/admin/users/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AdminUsersPage() {
+  return (
+    <main className="min-h-screen bg-[#050816] text-white p-10">
+      <h1 className="text-4xl font-black">
+        Admin Users
+      </h1>
+    </main>
+  );
+}
